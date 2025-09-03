6b0332e9063b61be1fe9a1f8dc0e6480587ff452
dd9971 <daniel.dlugosz@interia.pl>
2025-09-03 22:26:31 +0200

    Initialize task module for angular

---
+++ frontend/src/task/task.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  imports: [],
+  templateUrl: './task.html',
+  styleUrl: './task.css'
+})
+export class Task {
+
+let tasks = new ArrayList()
+}
